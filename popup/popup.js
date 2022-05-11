b8d0af79f068950a9a1c9b0edf513c271fc74d12
Fuyuzora <rwan7922@gmail.com>
2022-05-11 11:56:12 -0400

    mocked a response to programmatically set html content, added a topbar icon

--- popup/popup.js
+++ popup/popup.js
@@ -3,26 +3,51 @@
 
 // post text
 // TODO: post text to backend and fetch content
+url = chrome.runtime.getURL('popup/mock_resp.json')
 
 // display content
-import * as resp from "./mock_resp.json"
-
 let createDiv = className => {
-    div = document.createElement('div');
-    div.classList.add(className);
+    div = document.createElement('div')
+    div.classList.add(className)
     return div
 }
 
-mainContent = document.getElementsByClassName("main-content")[0]
-for (let type in resp) {
+let generateKeywordsSection = (data, type) => {
     sectionWrapper = createDiv("section-wrapper")
     header = createDiv("header")
-    header.innerHtml = type
-    for (let item in type) {
-        card = createDiv('card')
-        
+    header.innerHTML = type
+    sectionWrapper.appendChild(header)
+    console.log(data)
+    for (let ind in data) {
+        item = data[ind]
+        console.log(item.header)
+        let card = createDiv('card')
+        let contentHeader = createDiv("content-header")
+        let contentSubheader = createDiv("content-subheader")
+        let contentItem = createDiv("content-item")
+        contentHeader.innerHTML = item.header
+        contentSubheader.innerHTML = item.subheader
+        contentItem.innerHTML = item.content
+        contentHeader.appendChild(contentSubheader)
+        card.appendChild(contentHeader)
+        card.appendChild(contentItem)
+        sectionWrapper.appendChild(card)
+    }
+    return sectionWrapper
+}
+
+generatePage = (resp) => {
+    mainContent = document.getElementsByClassName("main-content")[0]
+    for (let type in resp) {
+        if (type === 'keywords') {
+            let section = generateKeywordsSection(resp[type], type)
+            mainContent.appendChild(section)
+        }
     }
 }
+
+fetch(url).then(resp => resp.json()).then(resp => generatePage(resp))
+
 // changeColor.addEventListener("click", async () => {
 //     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
