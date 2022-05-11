b07de25e6dd1563216185a1ca2a4bf5879d09e0f
Fuyuzora <rwan7922@gmail.com>
2022-05-11 17:26:55 -0400

    added skeleton loader

--- popup/popup.js
+++ popup/popup.js
@@ -1,30 +1,28 @@
-// gather text
-// TODO: create content script to gather text and send it to popup
-
 // post text
 // TODO: post text to backend and fetch content
 url = chrome.runtime.getURL('popup/mock_resp.json')
 
 // display content
-let createDiv = className => {
+let createDiv = (className, loader) => {
     div = document.createElement('div')
     div.classList.add(className)
+    if (loader) {
+        div.classList.add("skeleton-loader")
+    }
     return div
 }
 
 let generateKeywordsSection = (data, type) => {
-    sectionWrapper = createDiv("section-wrapper")
-    header = createDiv("header")
+    sectionWrapper = createDiv("section-wrapper", false)
+    header = createDiv("header", true)
     header.innerHTML = type
     sectionWrapper.appendChild(header)
-    console.log(data)
     for (let ind in data) {
         item = data[ind]
-        console.log(item.header)
-        let card = createDiv('card')
-        let contentHeader = createDiv("content-header")
-        let contentSubheader = createDiv("content-subheader")
-        let contentItem = createDiv("content-item")
+        let card = createDiv('card', false)
+        let contentHeader = createDiv("content-header", true)
+        let contentSubheader = createDiv("content-subheader", true)
+        let contentItem = createDiv("content-item", true)
         contentHeader.innerHTML = item.header
         contentSubheader.innerHTML = item.subheader
         contentItem.innerHTML = item.content
@@ -36,8 +34,8 @@ let generateKeywordsSection = (data, type) => {
     return sectionWrapper
 }
 
-generatePage = (resp) => {
-    mainContent = document.getElementsByClassName("main-content")[0]
+let generatePage = (resp) => {
+    let mainContent = document.getElementsByClassName("main-content")[0]
     for (let type in resp) {
         if (type === 'keywords') {
             let section = generateKeywordsSection(resp[type], type)
@@ -46,7 +44,33 @@ generatePage = (resp) => {
     }
 }
 
-fetch(url).then(resp => resp.json()).then(resp => generatePage(resp))
+let generateSkeletionLoader = (data) => {
+    data = data["keywords"]
+    sectionWrapper = createDiv("section-wrapper", false)
+    header = createDiv("header", true)
+    sectionWrapper.appendChild(header)
+    for (let ind in data) {
+        item = data[ind]
+        let card = createDiv('card', false)
+        let contentHeader = createDiv("content-header", true)
+        let contentSubheader = createDiv("content-subheader", true)
+        let contentItem = createDiv("content-item", true)
+        contentHeader.appendChild(contentSubheader)
+        card.appendChild(contentHeader)
+        card.appendChild(contentItem)
+        sectionWrapper.appendChild(card)
+    }
+    let placeholder = document.getElementsByClassName("placeholder")[0]
+    placeholder.appendChild(sectionWrapper)
+}
+
+let removeSkeletonLoader = () => {
+    let mainContent = document.getElementsByClassName("main-content")[0]
+    let placeholder = document.getElementsByClassName("placeholder")[0]
+    mainContent.removeChild(placeholder)
+}
+
+fetch(url).then(resp => resp.json()).then(resp => {generateSkeletionLoader(resp)})
 
 // changeColor.addEventListener("click", async () => {
 //     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
